ffce5753ec36198ac3baf7c847655612a37cedfa
Neirth <7413024+Neirth@users.noreply.github.com>
2021-04-15 13:04:32 +0200

    Añadido lógica con GPIO de la aplicación

--- src/ExpoSellerClient/app/src/main/java/io/smartinez/exposeller/client/peripherals/insertcoins/ButtonInsertCoinsImpl.java
+++ src/ExpoSellerClient/app/src/main/java/io/smartinez/exposeller/client/peripherals/insertcoins/ButtonInsertCoinsImpl.java
@@ -1,4 +1,42 @@
 package io.smartinez.exposeller.client.peripherals.insertcoins;
 
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
+
 public class ButtonInsertCoinsImpl implements IInsertCoins {
+    private PeripheralManager mPeripheralManager = PeripheralManager.getInstance();
+
+    public LiveData<Float> checkInsertedValue() throws IOException {
+        Gpio gpio = mPeripheralManager.openGpio("");
+
+        MutableLiveData<Float> countedValue = new MutableLiveData<>();
+        countedValue.setValue(0f);
+
+        gpio.registerGpioCallback((gpioAux) -> {
+            try {
+                if (gpioAux.getValue()) {
+                    countedValue.setValue(countedValue.getValue() + 0.50f);
+                }
+
+                return gpioAux.getValue();
+            } catch (IOException e) {
+                return false;
+            }
+        });
+
+        return countedValue;
+    }
+
+    public boolean returnExcessAmount(float desiredValue, float giveValue) {
+        Log.d("InsertCoins", "I'm a dummy method, checking if giveValue is greater or equal than desired value");
+
+        return giveValue >= desiredValue;
+    }
 }
--- src/ExpoSellerClient/app/src/main/java/io/smartinez/exposeller/client/peripherals/insertcoins/IInsertCoins.java
+++ src/ExpoSellerClient/app/src/main/java/io/smartinez/exposeller/client/peripherals/insertcoins/IInsertCoins.java
@@ -1,4 +1,10 @@
 package io.smartinez.exposeller.client.peripherals.insertcoins;
 
+import androidx.lifecycle.LiveData;
+
+import java.io.IOException;
+
 public interface IInsertCoins {
+    LiveData<Float> checkInsertedValue() throws IOException;
+    boolean returnExcessAmount(float desiredValue, float giveValue);
 }
