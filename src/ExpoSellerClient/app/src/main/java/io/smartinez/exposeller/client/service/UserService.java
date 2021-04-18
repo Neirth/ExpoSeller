67fa4d710c557ff46dc08586b103389ba84cf6a7
Neirth <7413024+Neirth@users.noreply.github.com>
2021-04-18 22:47:37 +0200

    Preparado usos de caso del usuario de la aplicación cliente

---
+++ src/ExpoSellerClient/app/src/main/java/io/smartinez/exposeller/client/service/UserService.java
@@ -0,0 +1,31 @@
+package io.smartinez.exposeller.client.service;
+
+import java.util.Date;
+
+import io.smartinez.exposeller.client.domain.Ticket;
+
+public class UserService {
+    public void checkInsertCoins() {
+
+    }
+
+    public void returnValueCoins() {
+
+    }
+
+    public void buyTicket(Ticket ticket) {
+
+    }
+
+    public void searchWithDay(Date date) {
+
+    }
+
+    public void readConcertList() {
+
+    }
+
+    public void pickRandomAdsList() {
+        
+    }
+}
