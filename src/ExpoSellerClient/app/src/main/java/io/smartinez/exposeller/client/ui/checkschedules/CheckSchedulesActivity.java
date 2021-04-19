ad51f84ec2fa52331c236e44fc10528483cec8a0
Neirth <7413024+Neirth@users.noreply.github.com>
2021-04-19 11:12:39 +0200

    Añadido propiedad en el manifiesto para mostrar en pantalla completa

--- src/ExpoSellerClient/app/src/main/java/io/smartinez/exposeller/client/ui/checkschedules/CheckSchedulesActivity.java
+++ src/ExpoSellerClient/app/src/main/java/io/smartinez/exposeller/client/ui/checkschedules/CheckSchedulesActivity.java
@@ -12,11 +12,7 @@ import android.widget.CalendarView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.time.Month;
-import java.time.Year;
 import java.time.format.TextStyle;
 import java.util.Calendar;
 import java.util.Date;
@@ -33,7 +29,7 @@ public class CheckSchedulesActivity extends AppCompatActivity {
     private RecyclerView mRvConcertList;
     private Guideline mGlHorizontalSeparator;
     private ImageView mIvExpoSellerLogo2;
-    private TextView mTvTitlePickup;
+    private TextView mTvTitleCheckSchedules;
     private Button mBtnCancelOperation1;
     private ConstraintLayout mClCalendar;
     private CalendarView mCvCalendar;
@@ -63,7 +59,7 @@ public class CheckSchedulesActivity extends AppCompatActivity {
         mRvConcertList = findViewById(R.id.rvConcertList);
         mGlHorizontalSeparator = findViewById(R.id.glHorizontalSeparator);
         mIvExpoSellerLogo2 = findViewById(R.id.ivExpoSellerLogo2);
-        mTvTitlePickup = findViewById(R.id.tvTitlePickup);
+        mTvTitleCheckSchedules = findViewById(R.id.tvTitleCheckSchedules);
         mBtnCancelOperation1 = findViewById(R.id.btnCancelOperation1);
         mClCalendar = findViewById(R.id.clCalendar);
         mCvCalendar = findViewById(R.id.cvCalendar);
@@ -73,7 +69,7 @@ public class CheckSchedulesActivity extends AppCompatActivity {
         mCvCalendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
             String monthStr = Month.of(month + 1).getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault()).toLowerCase();
 
-            mTvTitlePickup.setText(String.format(getString(R.string.check_schedules), monthStr, year));
+            mTvTitleCheckSchedules.setText(String.format(getString(R.string.check_schedules), monthStr, year));
         });
 
         Calendar calendar = Calendar.getInstance();
@@ -82,7 +78,7 @@ public class CheckSchedulesActivity extends AppCompatActivity {
         String monthStr = Month.of(calendar.get(Calendar.MONTH) + 1).getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault()).toLowerCase();
         Integer yearInt = calendar.get(Calendar.YEAR);
 
-        mTvTitlePickup.setText(String.format(getString(R.string.check_schedules), monthStr, yearInt));
+        mTvTitleCheckSchedules.setText(String.format(getString(R.string.check_schedules), monthStr, yearInt));
     }
 
     @Override
